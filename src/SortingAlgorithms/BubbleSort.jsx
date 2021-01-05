045de0ef2f33b882f8cbde2bf36ef53d52557023
Brendan Lucich <brendanlucich@gmail.com>
2021-01-05 16:58:21 -0500

    Update Bubble Sort

--- src/SortingAlgorithms/BubbleSort.jsx
+++ src/SortingAlgorithms/BubbleSort.jsx
@@ -1,5 +1,18 @@
 export function getBubbleSortAnimations(array) {
     const animations = [];
-    
+    n = array.length();
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; i < n-i-1; j++) {
+            animations.push([j, j+1]);
+            if (array[j] > array[j+1]) {
+                // swap
+                temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = array[j];
+                animations.push([i, j]);
+            }
+            animations.push([i, j])
+        }
+    }
     return animations;
 }
