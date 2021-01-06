c8db2432262e222ddce99d6f3226153dfe0babfe
Brendan Lucich <brendanlucich@gmail.com>
2021-01-05 22:34:12 -0500

    Bubble Sort
    But Only Visualizes the Changes... ALSO BUBBLE SORT IS SLOW #O(N^2)

--- src/SortingAlgorithms/BubbleSort.jsx
+++ src/SortingAlgorithms/BubbleSort.jsx
@@ -1,27 +1,33 @@
 export function getBubbleSortAnimations(array) {
 
     const animations = [];
-    console.log(array)
-    let n = array.length();
+    if (array.length <= 1) return array;
 
-    for (let i = 0; i < n-1; i++) {
-        // i is for the elements already sorted at the end
-        for (let j = 0; i < n-i-1; j++) {
-            // the two values we are comparing
-            animations.push([j, j+1]);
-            // the two values we are comparing
+    bubbleSort(array, animations);
+
+    return animations;
+
+    // console.log(array)
+}
+
+function swap(j, array) {
+    // swap
+    let temp = array[j];
+    array[j] = array[j+1];
+    array[j+1] = temp;
+}
+
+function bubbleSort(array, animations) {
+    let arrayLength = array.length;
+    for (let i = 0; i < arrayLength - 1; i++) {
+        for (let j = 0; j < arrayLength - i - 1; j++) {
+            // animations.push([j, j+1])
             if (array[j] > array[j+1]) {
-                // swap
-                // temp = array[j];
-                // array[j] = array[j+1];
-                // array[j+1] = array[j];
-                animations.push([j+1, j]);
-            }
-            else {
-                // no swap
+                swap(j, array);
                 animations.push([j, j+1]);
             }
         }
     }
-    return animations;
+    // console.log("ARRAY")
+    // console.log(array);
 }
--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -6,6 +6,12 @@ import {getBubbleSortAnimations} from '../SortingAlgorithms/BubbleSort.jsx';
 const VIS_MIN = 5
 const VIS_MAX = 650
 const ARR_SIZE = 100
+const ANIMATION_SPEED_MS = 50
+
+const PRIMARY_COLOR = '#6cc3d5'
+const SECONDARY_COLOR = '#fd7e14'
+const SWAP_COLOR = '#56cc9d'
+
 
 export default class SortingVisualizer extends React.Component {
     constructor(props) {
@@ -30,23 +36,54 @@ export default class SortingVisualizer extends React.Component {
         this.setState({array});
     }
 
-    animate
+    sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 
     //selection sort
     animateSelectionSort(){
 
     }
     //bubble sort
-    animateBubbleSort(){
-        let animations = getBubbleSortAnimations(this.render.array);
-        const color1 = '#6cc3d5';
-        const color2 = 'red';
-        const len = animations.length;
-        for (let i = 0; i < len; i++) {
-            console.log('Here')
+    async animateBubbleSort() {
+        const animations = getBubbleSortAnimations(this.state.array);
+        // console.log(animations);
+
+        for (let i = 0; i , animations.length; i++) {
+            let arrayBars = document.getElementsByClassName('array-bar');
+            // highlight then swap
+            
+            
+            // get indexes and styles of array values
+            // console.log(animations[i])
+            let [firstBar, secondBar] = animations[i];
+            // console.log("first bar")
+            // console.log(firstBar);
+            // console.log("second bar")
+            // console.log(secondBar);
+
+            // console.log(arrayBars)
+            let firstBarStyle = arrayBars[firstBar].style;
+            let secondBarStyle = arrayBars[firstBar + 1].style;
+
+            // highlight values being compared
+            firstBarStyle.backgroundColor = SECONDARY_COLOR;
+            secondBarStyle.backgroundColor = SECONDARY_COLOR;
+            await this.sleep(ANIMATION_SPEED_MS);
+
+            // show swap if there is one
+            firstBarStyle.backgroundColor = SWAP_COLOR;
+            secondBarStyle.backgroundColor = SWAP_COLOR;
+            const newHeight = firstBarStyle.height;
+            firstBarStyle.height = secondBarStyle.height;
+            secondBarStyle.height = newHeight;
+            await this.sleep(ANIMATION_SPEED_MS);
+
+            // unhighlight
+            firstBarStyle.backgroundColor = PRIMARY_COLOR;
+            secondBarStyle.backgroundColor = PRIMARY_COLOR;
+            await this.sleep(ANIMATION_SPEED_MS);
         }
-
-
     }
 
     //insertion sort
